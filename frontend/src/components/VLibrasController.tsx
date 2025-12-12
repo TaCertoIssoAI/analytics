3e1c830a385b95c297e7cb144209a9aaf2bcfc4f
pedrohfsilva <61637621+pedrohfsilva@users.noreply.github.com>
2025-12-11 23:40:05 -0300

    fix: vlibras no mobile

--- frontend/src/components/VLibrasController.tsx
+++ frontend/src/components/VLibrasController.tsx
@@ -1,6 +1,4 @@
-import { useState, useEffect } from "react";
-// @ts-ignore
-import VLibras from "vlibras-react";
+import { useEffect, useState } from "react";
 
 export const VLibrasController = () => {
   const [enabled, setEnabled] = useState(false);
@@ -10,5 +8,41 @@ export const VLibrasController = () => {
     setEnabled(vlibrasState);
   }, []);
 
-  return enabled ? <VLibras forceOnload={true} /> : null;
+  useEffect(() => {
+    if (enabled) {
+      const scriptSrc = "https://vlibras.gov.br/app/vlibras-plugin.js";
+      
+      if (!document.querySelector(`script[src="${scriptSrc}"]`)) {
+        const script = document.createElement("script");
+        script.src = scriptSrc;
+        script.async = true;
+        script.onload = () => {
+          // @ts-ignore
+          if (window.VLibras) {
+            // @ts-ignore
+            new window.VLibras.Widget('https://vlibras.gov.br/app');
+          }
+        };
+        document.body.appendChild(script);
+      } else {
+        // @ts-ignore
+        if (window.VLibras) {
+          // @ts-ignore
+          new window.VLibras.Widget('https://vlibras.gov.br/app');
+        }
+      }
+    }
+  }, [enabled]);
+
+  if (!enabled) return null;
+
+  return (
+    // @ts-ignore
+    <div vw="true">
+      <div vw-access-button="true" className="active"></div>
+      <div vw-plugin-wrapper="true">
+        <div className="vw-plugin-top-wrapper"></div>
+      </div>
+    </div>
+  );
 };
